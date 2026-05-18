a53e2499002aa68a020631765d18ade956e96f68
Hrithik Sharma <hrithiks.19989@gmail.com>
2026-05-18 18:05:49 +0530

    FeedForward Neural Network DL code : a simple architecture from input to output layer

---
+++ Neural_Networks in DL.py
@@ -0,0 +1,33 @@
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+from keras import layers
+from keras import Sequential
+
+data = {
+    'feature1': [0.1, 0.2, 0.3, 0.4, 0.5],
+    'feature2': [0.5, 0.4, 0.3, 0.2, 0.1],
+    'label': [0, 0, 1, 1, 1]
+}
+
+df = pd.DataFrame(data)
+X = df[['feature1', 'feature2']].values
+y = df['label'].values
+
+model = Sequential()
+model.add(layers.Dense(8, input_dim=2, activation='relu'))
+model.add(layers.Dense(1, activation='sigmoid'))
+
+model.compile(loss='binary_crossentropy',
+              optimizer='adam', metrics=['accuracy'])
+
+model.fit(X, y, epochs=100, batch_size=1, verbose=1)
+
+model.fit(X, y, epochs=100, batch_size=1, verbose=1)
+
+test_data = np.array([[0.2, 0.4]])
+prediction = model.predict(test_data)
+predicted_label = (prediction > 0.5).astype(int)
+
+
+
