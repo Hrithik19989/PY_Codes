b19865db34295a5e137fee06b897e3ea3f2ab2e6
Hrithik Sharma <hrithiks.19989@gmail.com>
2026-05-25 18:07:28 +0530

    Pooling Layer in CNN

--- Pooling Layers and its types in CNN in DL.py
+++ Pooling Layers and its types in CNN in DL.py
@@ -2,6 +2,7 @@ import tensorflow as tf
 from keras.layers import MaxPooling2D ,AveragePooling2D , GlobalMaxPooling2D, GlobalAveragePooling2D
 import numpy as np
 
+#Feature for MaxPooling2D
 feature_map_mp = np.array([
     [1, 3, 2, 9],
     [5, 6, 1, 7],
@@ -9,6 +10,7 @@ feature_map_mp = np.array([
     [3, 5, 7, 2]
 ]).reshape(1, 4, 4, 1)
 
+# Feature for everything else
 feature_map_GMAP = np.array([
     [1, 3, 2, 9],
     [5, 6, 1, 7],
@@ -17,19 +19,23 @@ feature_map_GMAP = np.array([
 ], dtype=np.float32).reshape(1, 4, 4, 1)
 
 
-
+#MaxPooling2D
 max_pool = MaxPooling2D(pool_size=(2, 2), strides=2)
 output_mp = max_pool(feature_map_mp)
 
+#AveragePooling2D
 avg_pool = AveragePooling2D(pool_size=(2, 2), strides=2)
 output_ap = avg_pool(feature_map_GMAP)
 
+#GlobalMaxPooling2D
 gm_pool = GlobalMaxPooling2D()
 gm_output_GMP = gm_pool(feature_map_GMAP)
 
+#GlobalAveragePooling2D
 ga_pool = GlobalAveragePooling2D()
 ga_output_GAP = ga_pool(feature_map_GMAP)
 
+#Printing Outputs
 print(output_mp.numpy().reshape(2, 2))
 print(output_ap.numpy().reshape(2, 2))
 print("Global Max Pooling Output:", gm_output_GMP.numpy())
