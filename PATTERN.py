a8ff9841078c81e46ce499cb363aa83ec36621c0
Hrithik Sharma <hrithiks.19989@gmail.com>
2026-05-26 18:05:42 +0530

    Pattern codees

--- PATTERN.py
+++ PATTERN.py
@@ -1,8 +1,13 @@
-n = 9
-m = 27
 
-for i in range(n+1):
-    pattern = "!*!" * (i)
-    p1 = pattern.ljust(m , ":")
-    p2 = pattern.rjust(m , ":")
-    print(p1 + p2)
+size = 3
+n = size - 1  # Maximum distance from center (2)
+
+for x in range(-n, n + 1):
+    row_chars = []
+    for y in range(-n, n + 1):
+        distance = abs(x) + abs(y)
+        if distance > n:
+            row_chars.append("-")
+        else:
+            row_chars.append(chr(97 + distance))
+    print("-".join(row_chars))
